ce22753756ac4b7e1fa39885a36d567b5ef2b81f
fel1200 <fel1200@hotmail.com>
2024-06-03 14:36:46 -0600

    Version previa para las tiendas, aún faltando corregir las URLs finales

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from "react";
+//For navigation
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./src/navigation/Navigation";
+//For the font loading
+import { useFonts } from "expo-font";
+//For context and get general data independent of the screen
+import { AppProvider } from "./src/context/AppContext";
 
 export default function App() {
+  //Fonts popins loading
+  const [fontsLoaded] = useFonts({
+    "Poppins-regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-italic": require("./assets/fonts/Poppins-Italic.ttf"),
+    "Poppins-bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-semibold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-light": require("./assets/fonts/Poppins-Light.ttf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    console.log("Not fonts loaded");
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppProvider>
+        <Navigation />
+      </AppProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
